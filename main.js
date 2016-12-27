0d863aaac0f3fcc2cd67abdac0fc4a331cba1d7e
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2016-12-27 11:35:05 -0500

    javascript terminado

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ function evaluar(){
     var loroHabla = document.getElementById("loro_habla").value;
     var habla = true;
     var salida = document.getElementById("salida");
-    
+        
     if (loroHabla == "2"){
         habla = false;
     }
     if(horaActual < 7 || horaActual > 20){
-        if (loroHabla = true){
+        if (habla == true){
             salida.innerHTML = "si";
         }
-        else {
+        else if(habla == false){
             salida.innerHTML = "no";
         }
     }
@@ -19,3 +19,25 @@ function evaluar(){
         salida.innerHTML = "no";
     }
 }
+
+
+/* --------- otra solucion ---- */
+/*
+function evaluar(){
+    var horaActual = document.getElementById("hora_actual").value;
+    var loroHabla = document.getElementById("loro_habla").value;
+    var salida = document.getElementById("salida");
+    
+    if(horaActual < 7 || horaActual > 20){
+        if (loroHabla == "1"){
+            salida.innerHTML = "si";
+        }
+        else if(loroHabla == "2"){
+            salida.innerHTML = "no";
+        }
+    }
+    else {
+        salida.innerHTML = "no";
+    }
+}
+*/
